f384239b29f67940a54848e6846e09211f2f4c42
naoufal jrhaider <naoufal_jrh@naoufals-MacBook-Pro.local>
2025-03-11 14:47:15 +0000

    first commit

---
+++ src/main/java/com/poc/linkedinqapoc/LinkedInQaPoCApplication.java
@@ -0,0 +1,13 @@
+package com.poc.linkedinqapoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedInQaPoCApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkedInQaPoCApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/poc/linkedinqapoc/LinkedInQaPoCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.linkedinqapoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedInQaPoCApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
