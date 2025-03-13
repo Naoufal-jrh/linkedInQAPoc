cb59b1df928cf35d727cdc11ddebe8ec1abefc73
naoufal jrhaider <naoufal_jrh@naoufals-MacBook-Pro.local>
2025-03-13 12:52:56 +0000

    implemented the Scraper Service and the web driver bean

---
+++ src/main/java/com/poc/linkedinqapoc/scraper/ScraperService.java
@@ -0,0 +1,114 @@
+package com.poc.linkedinqapoc.scraper;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.List;
+
+@Service
+public class ScraperService {
+    private final WebDriver driver;
+    private static final int WAIT_FOR_ELEMENT_TIMEOUT = 5;
+    private static final String TOP_CARD = "pv-top-card";
+
+    public ScraperService(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public static void wait(int duration) {
+        try {
+            Thread.sleep(duration * 1000L);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public void focus() {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("alert('Focus window')");
+        driver.switchTo().alert().accept();
+    }
+
+    public void mouseClick(WebElement elem) {
+        Actions action = new Actions(driver);
+        action.moveToElement(elem).perform();
+    }
+
+    public WebElement waitForElementToLoad(By by, String name, WebElement base) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(WAIT_FOR_ELEMENT_TIMEOUT));
+        return wait.until(ExpectedConditions.presenceOfElementLocated(By.className(name)));
+    }
+
+    public List<WebElement> waitForAllElementsToLoad(By by, String name, WebElement base) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(WAIT_FOR_ELEMENT_TIMEOUT));
+        return wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.className(name)));
+    }
+
+    public boolean isSignedIn() {
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(WAIT_FOR_ELEMENT_TIMEOUT));
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.className("VERIFY_LOGIN_ID")));
+            driver.findElement(By.className("VERIFY_LOGIN_ID"));
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public void scrollToHalf() {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollTo(0, Math.ceil(document.body.scrollHeight/2));");
+    }
+
+    public void scrollToBottom() {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
+    }
+
+    public void scrollClassNameElementToPagePercent(String className, float pagePercent) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("var elem = document.getElementsByClassName('" + className + "')[0]; elem.scrollTo(0, elem.scrollHeight * " + pagePercent + ");");
+    }
+
+    public boolean findElementByClassName(String className) {
+        try {
+            driver.findElement(By.className(className));
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    public boolean findElementByXpath(String tagName) {
+        try {
+            driver.findElement(By.xpath(tagName));
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    public boolean findEnabledElementByXpath(String tagName) {
+        try {
+            WebElement elem = driver.findElement(By.xpath(tagName));
+            return elem.isEnabled();
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    public static WebElement findFirstAvailableElement(WebElement... elements) {
+        for (WebElement elem : elements) {
+            if (elem != null) {
+                return elem;
+            }
+        }
+        return null;
+    }
+
+
+}
---
+++ src/main/java/com/poc/linkedinqapoc/scraper/WebDriverConfig.java
@@ -0,0 +1,14 @@
+package com.poc.linkedinqapoc.scraper;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebDriverConfig {
+    @Bean
+    public WebDriver getWebDriver() {
+        return new ChromeDriver();
+    }
+}
