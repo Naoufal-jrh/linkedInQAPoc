6c80acc46a88c554b5bd62b88961b3a1d6c109df
naoufal jrhaider <naoufal_jrh@dhcp-10-154-35-175.vpn.oracle.com>
2025-03-18 10:41:30 +0000

    removed all the unused functions and properties from ScraperService

--- src/main/java/com/poc/linkedinqapoc/scraper/ScraperService.java
+++ src/main/java/com/poc/linkedinqapoc/scraper/ScraperService.java
@@ -18,7 +18,6 @@ import java.util.List;
 public class ScraperService {
     private final WebDriver driver;
     private static final int WAIT_FOR_ELEMENT_TIMEOUT = 5;
-    private static final String TOP_CARD = "pv-top-card";
 
     public static void wait(int duration) {
         try {
@@ -34,20 +33,6 @@ public class ScraperService {
         driver.switchTo().alert().accept();
     }
 
-    public void mouseClick(WebElement elem) {
-        Actions action = new Actions(driver);
-        action.moveToElement(elem).perform();
-    }
-
-    public WebElement waitForElementToLoad(By by, String name, WebElement base) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(WAIT_FOR_ELEMENT_TIMEOUT));
-        return wait.until(ExpectedConditions.presenceOfElementLocated(By.className(name)));
-    }
-
-    public List<WebElement> waitForAllElementsToLoad(By by, String name, WebElement base) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(WAIT_FOR_ELEMENT_TIMEOUT));
-        return wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.className(name)));
-    }
 
     public boolean isSignedIn() {
         System.out.println("checking if signed in");
@@ -62,69 +47,6 @@ public class ScraperService {
             return true;
         }
     }
-
-    public void scrollToHalf() {
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("window.scrollTo(0, Math.ceil(document.body.scrollHeight/2));");
-    }
-
-    public void scrollToBottom() {
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
-    }
-
-    public void scrollClassNameElementToPagePercent(String className, float pagePercent) {
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("var elem = document.getElementsByClassName('" + className + "')[0]; elem.scrollTo(0, elem.scrollHeight * " + pagePercent + ");");
-    }
-
-    public boolean findElementByClassName(String className) {
-        try {
-            driver.findElement(By.className(className));
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
-
-    public boolean findElementByXpath(String tagName) {
-        try {
-            driver.findElement(By.xpath(tagName));
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
-
-    public boolean findElementByTagName(String tagName) {
-        try {
-            driver.findElement(By.tagName(tagName));
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
-
-
-    public boolean findEnabledElementByXpath(String tagName) {
-        try {
-            WebElement elem = driver.findElement(By.xpath(tagName));
-            return elem.isEnabled();
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
-
-    public static WebElement findFirstAvailableElement(WebElement... elements) {
-        for (WebElement elem : elements) {
-            if (elem != null) {
-                return elem;
-            }
-        }
-        return null;
-    }
-
-
 
     public void open(String url) {
         System.out.println("opening " + url);
